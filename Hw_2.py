e9e0eeb9aaa1f21bdf90ccc1d6007fedd88473ca
Ivan <magasalliwan@gmail.com>
2025-07-13 22:14:30 +0300

    Додано коментарі

--- Hw_2.py
+++ Hw_2.py
@@ -1,20 +1,38 @@
-import re # Імпортуємо модуль 
+# Імпортуємо модуль
+import re  
 
 # text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 
 # Функція-генератор, яка знаходить числа у тексті
-def generator_numbers(text): 
-    pattern = r"\b\d+\.\d+|\d+\b" # Шаблон для пошуку чисел
-    numbers = re.findall(pattern, text) # Пошук чисел
+def generator_numbers(text):
+
+    # Шаблон для пошуку чисел
+    pattern = r"\b\d+\.\d+|\d+\b"
+
+    # Пошук чисел
+    numbers = re.findall(pattern, text) 
     for number in numbers:
-        yield number # Повертаємо числа по черзі
+
+        # Повертаємо числа по черзі
+        yield number 
 
 # Функція для розрахунку суми чисел 
 def sum_profit(text, func):
-    number_sum = 0 # Початкова сума
-    for number in func(text): # Проходимо по всих числах з генератора
-        number_sum += float(number) # Робимо з рядка число і додаєм до суми
-    return number_sum   # Повертаємо загальний дохід   
 
-total_income = sum_profit(text, generator_numbers) # Викликаємо функцію і передаємо Ій данні
-print(f"Загальний дохід: {total_income}") # Виводимо загальний дохід
+    # Початкова сума
+    number_sum = 0
+
+    # Проходимо по всих числах з генератора
+    for number in func(text):
+
+        # Робимо з рядка число і додаєм до суми
+        number_sum += float(number)
+
+    # Повертаємо загальний дохід
+    return number_sum      
+
+# Викликаємо функцію і передаємо Ій данні
+total_income = sum_profit(text, generator_numbers)
+
+# Виводимо загальний дохід
+print(f"Загальний дохід: {total_income}")
